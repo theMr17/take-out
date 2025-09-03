c1b58e485f44809b3758e8e3b8d6cda63fabc31e
Saptak Manna <saptakmanna100@gmail.com>
2025-09-03 15:15:33 +0530

    Refactor light flickering functionality
    - Increased the time between intensity changes in FlickeringLight from 0.1f to 0.15f for smoother transitions.
    - Removed the LightFlicker script as its functionality is now handled by FlickeringLight.

--- Assets/Scripts/FlickeringLight.cs
+++ Assets/Scripts/FlickeringLight.cs
@@ -8,7 +8,7 @@ public class FlickeringLight : MonoBehaviour
 
   [SerializeField, Range(0f, 3f)] private float minIntensity = 0.5f;
   [SerializeField, Range(0f, 3f)] private float maxIntensity = 1.2f;
-  [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.1f;
+  [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.15f;
 
   private float currentTimer;
 
